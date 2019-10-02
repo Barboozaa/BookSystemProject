0ad1ee2d7263f793c28e705aba7239c5608ef372
Patrick Hussey <husspm@gmail.com>
2019-10-02 15:45:49 -0400

    Service Layer almost done

--- book-service/src/main/java/com/company/bookservice/viewModel/BookViewModel.java
+++ book-service/src/main/java/com/company/bookservice/viewModel/BookViewModel.java
@@ -3,12 +3,14 @@ package com.company.bookservice.viewModel;
 import com.company.bookservice.dto.Book;
 import com.company.bookservice.util.messages.Note;
 
+import java.util.List;
 import java.util.Objects;
 
 public class BookViewModel {
 
     Book book;
     Note note;
+    List<Note> notes;
 
     public Book getBook() {
         return book;
@@ -26,13 +28,22 @@ public class BookViewModel {
         this.note = note;
     }
 
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        BookViewModel that = (BookViewModel) o;
-        return book.equals(that.book) &&
-                Objects.equals(note, that.note);
+        BookViewModel model = (BookViewModel) o;
+        return book.equals(model.book) &&
+                Objects.equals(note, model.note) &&
+                Objects.equals(notes, model.notes);
     }
 
     @Override
