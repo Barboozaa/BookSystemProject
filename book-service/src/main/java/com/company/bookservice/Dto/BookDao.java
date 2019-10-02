c6e62c484e2866022525ab124bb98202dee83247
ahmed <akahin@gmail.com>
2019-10-02 13:30:14 -0400

    added Notes

--- book-service/src/main/java/com/company/bookservice/Dto/BookDao.java
+++ book-service/src/main/java/com/company/bookservice/Dto/BookDao.java
@@ -1,10 +1,5 @@
 package com.company.bookservice.Dto;
 
-
- book_id int not null auto_increment primary key,
- title varchar(50) not null,
- author varchar(50) not null
-
 public class BookDao {
  @min(0)
  private int book_id;
@@ -13,4 +8,35 @@ public class BookDao {
  @Size(min=5, max=50)
  private String author;
 
+ public Bookdao(){}
+
+ public BookDao(int book_id, String title, String author) {
+  this.book_id = book_id;
+  this.title = title;
+  this.author = author;
+ }
+
+ public int getBook_id() {
+  return book_id;
+ }
+
+ public void setBook_id(int book_id) {
+  this.book_id = book_id;
+ }
+
+ public String getTitle() {
+  return title;
+ }
+
+ public void setTitle(String title) {
+  this.title = title;
+ }
+
+ public String getAuthor() {
+  return author;
+ }
+
+ public void setAuthor(String author) {
+  this.author = author;
+ }
 }
